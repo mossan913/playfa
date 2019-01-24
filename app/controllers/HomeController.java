cc856d4577bef49e8ed4c1c1961760074def1802
坂本翔 <0rp326091071c2x@gmail.com>
2019-01-25 00:58:58 +0900

    #6 #7 対応。

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.time.LocalDateTime;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -9,14 +11,14 @@ import play.mvc.Result;
  */
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
-    public Result index() {
-        return ok(views.html.index.render());
-    }
+	/**
+	 * An action that renders an HTML page with a welcome message.
+	 * The configuration in the <code>routes</code> file means that
+	 * this method will be called when the application receives a
+	 * <code>GET</code> request with a path of <code>/</code>.
+	 */
+	public Result index() {
+		return ok(views.html.index.render(LocalDateTime.now().toString()));
+	}
 
 }
